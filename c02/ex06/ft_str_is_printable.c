4066b0d7ac62660e5a734f2ec2ff29ff943b3c79
root <root@DESKTOP-AK55V1Q.localdomain>
2021-04-02 22:38:56 +0900

    c02 ex06 fixed

--- c02/ex06/ft_str_is_printable.c
+++ c02/ex06/ft_str_is_printable.c
@@ -10,14 +10,14 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-int		ft_str_is_printable(char *str)
+int	ft_str_is_printable(char *str)
 {
 	int i;
 
 	i = 0;
 	while (str[i] != '\0')
 	{
-		if (str[i] < 33 || 126 < str[i])
+		if (str[i] < 32 || str[i] == 127)
 			return (0);
 		i++;
 	}
