86a621a1cf1d820991e8fe287fdc44a5decdfd4e
sehkang <sehkang@student.42seoul.kr>
2021-05-05 15:49:28 +0900

    libft

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -1,42 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sehkang <sehkang@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/05/05 14:13:24 by sehkang           #+#    #+#             */
-/*   Updated: 2021/05/05 15:29:08 by sehkang          ###   ########.fr       */
+/*   Created: 2021/05/05 13:11:21 by sehkang           #+#    #+#             */
+/*   Updated: 2021/05/05 15:48:44 by sehkang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+size_t	strlcat(char *dst, const char *src, size_t dstsize)
 {
-	char	*h_ptr;
-	char	*n_ptr;
-	size_t	i;
+	size_t dst_len;
+	size_t src_len;
 
-	h_ptr = (char *)haystack;
-	n_ptr = (char *)needle;
-	i = 0;
-	if (*n_ptr == '\0')
-		return (h_ptr);
-	while (0 < len)
-	{
-		if (*h_ptr == *n_ptr)
-		{
-			while (h_ptr[i] == n_ptr[i] && i < len)
-			{
-				i++;
-				if (n_ptr[i] == '\0')
-					return (h_ptr);
-			}
-			i = 0;
-		}
-		h_ptr++;
-		len--;
-	}
-	return (NULL);
-}
+	dst_len = ft_strlen(dst);
+	src_len = ft_strlen(src);
+	ft_memcpy
--- libft.h
+++ libft.h
@@ -1,5 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sehkang <sehkang@student.42seoul.kr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/05 13:06:43 by sehkang           #+#    #+#             */
+/*   Updated: 2021/05/05 15:35:53 by sehkang          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
-#include <stddef.h>
+
+void	ft_bzero(void *s, size_t n);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
+void	*ft_memmove(void *dst, const void *src, size_t len);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+void	*ft_memchr(const void *s, int c, size_t n);
 void	*ft_memset(void *b, int c, size_t len);
+size_t	ft_strlen(const char *s);
+void	*ft_memcpy(void *dst, const void *src, size_t n);
+void	*ft_memccpy(void *dst, const void *src, int c, size_t n);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strrchr(const char *s, int c);
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
+size_t	strlcat(char *dst, const char *src, size_t dstsize);
+
+
+#endif
