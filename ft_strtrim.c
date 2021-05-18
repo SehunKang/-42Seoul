cdc8f548bdd7510f694dd634af2797accdade76b
sehkang <sehkang@student.42seoul.kr>
2021-05-18 14:43:29 +0900

    libft

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: sehkang <sehkang@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/06 16:56:39 by sehkang           #+#    #+#             */
-/*   Updated: 2021/05/11 16:43:12 by sehkang          ###   ########.fr       */
+/*   Updated: 2021/05/18 14:43:07 by sehkang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,11 +21,11 @@ char	*ft_strtrim(char const *s1, char const *set)
 	i = 0;
 	j = 0;
 	s1_len = ft_strlen(s1);
-	if (!s1)
+	if (s1 == NULL)
 		return (NULL);
 	while (s1[i] && ft_strchr(set, s1[i]))
 		i++;
-	while (s1[s1_len - j - 1] && ft_strchr(set, s1[s1_len - j - 1]))
+	while (s1_len - j > 0 && ft_strchr(set, s1[s1_len - j - 1]))
 		j++;
 	if (i + j < s1_len)
 		return (ft_substr(s1, i, s1_len - i - j));
