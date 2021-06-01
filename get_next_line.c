dc1ee84643b9a601a567114d47172def225a780f
SehunKang <andriy.kang@gmail.com>
2021-06-01 10:21:04 +0900

    GNL

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: sehkang <sehkang@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/17 15:05:29 by sehkang           #+#    #+#             */
-/*   Updated: 2021/05/30 15:33:52 by sehkang          ###   ########.fr       */
+/*   Updated: 2021/06/01 10:20:06 by sehkang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,41 +50,31 @@ int		ret_eof(char **str_backup, char **line)
 	return (0);
 }
 
-int		check_start_end(int fd, char **line, char *str_backup[][2])
-{
-	if (str_backup[fd][1] == NULL)
-	{
-		str_backup[fd][1] = "1";
-		str_backup[fd][0] = ft_strdup("");
-	}
-	if (str_backup[fd][0] == NULL)
-	{
-		*line = ft_strdup("");
-		return (0);
-	}
-	return (1);
-}
-
 int		get_next_line(int fd, char **line)
 {
-	static char *str_backup[OPEN_MAX][2];
+	static char *str_backup[OPEN_MAX];
 	char		buf[BUFFER_SIZE + 1];
 	ssize_t		r_bytes;
 
 	r_bytes = 1;
 	if (fd < 0 || OPEN_MAX < fd || line == NULL || BUFFER_SIZE <= 0)
 		return (-1);
-	if (check_start_end(fd, line, str_backup) == 0)
-		return (0);
-	while (!(ft_strchr(str_backup[fd][0], '\n')))
+	if ((r_bytes = read(fd, buf, BUFFER_SIZE)) >= 0)
+	{
+		buf[r_bytes] = '\0';
+		str_backup[fd] = buf_stack(&str_backup[fd], buf);
+	}
+	else
+		return (-1);
+	while (!(ft_strchr(str_backup[fd], '\n')))
 	{
 		if (r_bytes == 0)
-			return (ret_eof(&str_backup[fd][0], line));
+			return (ret_eof(&str_backup[fd], line));
 		if ((r_bytes = read(fd, buf, BUFFER_SIZE)) > 0)
 		{
 			buf[r_bytes] = '\0';
-			str_backup[fd][0] = buf_stack(&str_backup[fd][0], buf);
+			str_backup[fd] = buf_stack(&str_backup[fd], buf);
 		}
 	}
-	return (ret_val(&str_backup[fd][0], line));
+	return (ret_val(&str_backup[fd], line));
 }
